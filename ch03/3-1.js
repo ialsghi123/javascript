7237a015b9dd1c53642632df13478b88c778a11c
ialsghi123 <ialsghi123@naver.com>
2021-01-09 22:47:47 +0900

    2021-01-09

--- ch03/3-1.js
+++ ch03/3-1.js
@@ -1,3 +1,32 @@
+//자바스크립트 기본 타입
+
+
 //숫자 타입
 var intNum = 10;
 var floatNum = 0.1;
+
+//문자열 타입
+var singleQuoteStr = 'single quote string';
+var doubleQuoteStr = "double quote string";
+var singleChar = 'a';
+
+//불린 타입
+var boolVar = true;
+
+//underfined 타입
+var emptyvar;
+
+//null 타입
+var nullVar = null;
+
+console.log(
+    typeof intNum,
+    typeof floatNum,
+    typeof singleQuoteStr,
+    typeof doubleQuoteStr,
+    typeof singleChar,
+    typeof boolVar,
+    typeof nullVar,
+    typeof emptyvar
+
+);
---
+++ ch03/3-2.js
@@ -0,0 +1,7 @@
+//자바스크립트 나눗셈 연산
+
+
+var num = 5 / 2;
+
+console.log(num);
+console.log(Math.floor(num));
