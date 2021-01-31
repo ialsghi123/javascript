f9dcf17708ec8c9412d8d32fec7dc059dcdbce8d
ialsghi123 <ialsghi123@naver.com>
2021-01-31 23:39:45 +0900

    2021-01-31

--- ch04/4-21.js
+++ ch04/4-21.js
@@ -1,1 +1,10 @@
-//
+//함수 형식에 맞춰 인자를 넘기지 않더라도 함수 호출이 가능함을 나타내는 예제 코드
+
+function func(arg1, arg2) {
+    console.log(arg1, arg2);
+}
+
+func();   // undefined undefined
+func(1);  // 1 undefined
+func(1,2);  // 1 2
+func(1,2,3); // 1 2
--- ch04/4-28.js
+++ ch04/4-28.js
@@ -1,1 +1,10 @@
-//
+//생성자 함수의 동작 방식
+
+// Person 생성자 함수
+var Person = function (name) {
+    this.name = name;
+};
+
+// foo 객체 생성
+var foo = new Person('foo');
+console.log(foo.name); // foo
